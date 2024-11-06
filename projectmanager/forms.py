058fb32eff239e54b125e17fc707f8180e558062
m-triple-m <triplem656@gmail.com>
2024-11-07 01:46:39 +0530

    added student details

--- projectmanager/forms.py
+++ projectmanager/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     """Form definition for Project."""
@@ -9,3 +10,20 @@ class ProjectForm(ModelForm):
 
         model = Project
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(  attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'description': forms.Textarea(attrs={'rows': 5, 'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'department': forms.TextInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'technology': forms.TextInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'image': forms.FileInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'student_name': forms.TextInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'student_github': forms.URLInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'student_linkedin': forms.URLInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'student_email': forms.EmailInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'student_avatar': forms.FileInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            'enrollment_number': forms.TextInput(attrs={'class': 'w-full bg-gray-300 rounded px-4 py-2 mb-6 text-black'}),
+            
+        }
+        labels = {
+            'title': 'Project Title'
+        }
--- projectmanager/models.py
+++ projectmanager/models.py
@@ -8,6 +8,12 @@ class Project(models.Model):
     department = models.CharField(max_length=50, default='IT')
     technology = models.CharField(max_length=20)
     image = models.ImageField(upload_to='project_images/')
+    student_name = models.CharField(max_length=100, default='Anonymous')
+    student_github = models.URLField(max_length=200, default='')
+    student_linkedin = models.URLField(max_length=200, default='')
+    student_email = models.EmailField(max_length=100, default='')
+    student_avatar = models.ImageField(upload_to='student_avatars/', default='student_avatars/default.png')
+    enrollment_number = models.CharField(max_length=20, default='')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
